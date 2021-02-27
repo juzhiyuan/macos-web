61e081a2934e499902242fe233695b711cd0d220
Puru Vijay <devpuruvj@gmail.com>
2021-02-28 01:28:45 +0530

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { resolve } from 'path';
+import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    {
+      ...babel({ babelHelpers: 'bundled' }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+  ],
+  build: {
+    terserOptions: {
+      mangle: {
+        reserved: ['styled'],
+      },
+      parse: {},
+      keep_fnames: true,
+    },
+    minify: 'terser',
+  },
   esbuild: {
     jsxInject: `import React from 'react';`,
     minify: true,
